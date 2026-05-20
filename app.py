39169473b5c1c3cddccf33276517db0fdd9a7c82
Bhagavathkrishna S R <srbhagavathkrishna@gmail.com>
2026-05-20 23:02:05 +0530

    Added database initialization

--- app.py
+++ app.py
@@ -1055,11 +1055,11 @@ def ai_search():
 # MAIN
 # =========================================================
 
+with app.app_context():
+
+    db.create_all()
+
 if __name__ == "__main__":
-
-    with app.app_context():
-
-        db.create_all()
 
     app.run(debug=True)
 
