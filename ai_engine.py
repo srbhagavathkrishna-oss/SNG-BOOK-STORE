7da1a1f60c29bf29cbfa73f4955cd3b146e3aecb
Bhagavathkrishna S R <srbhagavathkrishna@gmail.com>
2026-05-20 22:34:56 +0530

    Removed heavy AI for deployment

--- ai_engine.py
+++ ai_engine.py
@@ -1,16 +1,3 @@
-import easyocr
-import cv2
-
-reader = easyocr.Reader(['en'])
-
 def extract_text(image_path):
 
-    results = reader.readtext(image_path)
-
-    text = ""
-
-    for result in results:
-
-        text += result[1] + " "
-
-    return text.strip()
+    return ""
